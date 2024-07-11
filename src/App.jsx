6b59d6905e2150c62f168def22f71de44b7db6be
Luis-AP <lap18958@gmail.com>
2024-07-10 21:19:35 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+    return (
+        <div className="container">
+            <h1 className="title">Mi app</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Products.jsx
@@ -0,0 +1,8 @@
+export default function Products() {
+    return (
+        <div>
+            <h1>Products</h1>
+            <p>Check out our amazing products!</p>
+        </div>
+    );
+}
